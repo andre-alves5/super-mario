428a40bb9655d73a865377ef8169421f6b015836
andre-alves5 <andrecambridge@gmail.com>
2020-04-28 13:28:53 -0300

    Aula 6

--- public/js/sprites.js
+++ public/js/sprites.js
@@ -3,17 +3,8 @@ import { loadImage } from "./loaders.js";
 
 export function loadMarioSprite() {
   return loadImage("/img/characters.gif").then((image) => {
-    const sprites = new SpriteSheet(image);
+    const sprites = new SpriteSheet(image, 16, 16);
     sprites.define("idle", 276, 44, 16, 16);
-    return sprites;
-  });
-}
-
-export function loadBackgroundSprites() {
-  return loadImage("/img/tiles.png").then((image) => {
-    const sprites = new SpriteSheet(image);
-    sprites.defineTile("ground", 0, 0);
-    sprites.defineTile("sky", 3, 23);
     return sprites;
   });
 }
