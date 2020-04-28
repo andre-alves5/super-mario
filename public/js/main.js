359a3bec15a287fab9a234e773aab4a9e7edcd46
andre-alves5 <andrecambridge@gmail.com>
2020-04-28 13:08:32 -0300

    Aula 5

--- public/js/main.js
+++ public/js/main.js
@@ -1,51 +1,37 @@
-import Compositor from "./Compositor.js";
 import Timer from "./Timer.js";
 import { loadLevel } from "./loaders.js";
 import { createMario } from "./entities.js";
-import { loadBackgroundSprites } from "./sprites.js";
-import { createBackgroundLayer, createSpriteLayer } from "./layers.js";
-
-import Keyboard from "./KeyboardState.js";
+import { createCollisionLayer } from "./layers.js";
+import { setupKeyboard } from "./input.js";
 
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
 
-Promise.all([createMario(), loadBackgroundSprites(), loadLevel("1-1")]).then(
-  ([mario, backgroundSprites, level]) => {
-    const comp = new Compositor();
+Promise.all([createMario(), loadLevel("1-1")]).then(([mario, level]) => {
+  mario.pos.set(64, 64);
 
-    const backgroundLayer = createBackgroundLayer(
-      level.backgrounds,
-      backgroundSprites
-    );
-    comp.layers.push(backgroundLayer);
+  level.comp.layers.push(createCollisionLayer(level));
 
-    const gravity = 2000;
-    mario.pos.set(64, 180);
+  level.entities.add(mario);
 
-    const SPACE = 32;
-    const input = new Keyboard();
-    input.addMapping(SPACE, (keyState) => {
-      if (keyState) {
-        mario.jump.start();
-      } else {
-        mario.jump.cancel();
+  const input = setupKeyboard(mario);
+  input.listenTo(window);
+
+  ["mousedown", "mousemove"].forEach((eventName) => {
+    canvas.addEventListener(eventName, (event) => {
+      if (event.buttons === 1) {
+        mario.vel.set(0, 0);
+        mario.pos.set(event.offsetX, event.offsetY);
       }
     });
-    input.listenTo(window);
+  });
 
-    const spriteLayer = createSpriteLayer(mario);
-    comp.layers.push(spriteLayer);
+  const timer = new Timer(1 / 60);
+  timer.update = function update(deltaTime) {
+    level.update(deltaTime);
 
-    const timer = new Timer(1 / 60);
-    timer.update = function update(deltaTime) {
-      mario.update(deltaTime);
+    level.comp.draw(context);
+  };
 
-      comp.draw(context);
-
-      mario.vel.y += gravity * deltaTime;
-    };
-
-    timer.start();
-  }
-);
+  timer.start();
+});
